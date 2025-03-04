9cff8593db56799a3d3b763569a3a09d1e9c170d
Tyler <tyeknapp.pro@gmail.com>
2025-03-03 21:15:02 -0500

    Add some modules

--- dfc-fire-prevention/nuxt.config.ts
+++ dfc-fire-prevention/nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  modules: ['@nuxt/image', '@nuxt/test-utils', '@nuxt/eslint', '@nuxt/icon'],
 })
-
